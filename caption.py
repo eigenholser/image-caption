e7fef81da629f2ae40387a5fcb1373deeac436c5
Scott Overholser <eigenholser@users.noreply.github.com>
2017-12-24 20:40:22 -0700

    Clean up and comment code.

--- caption.py
+++ caption.py
@@ -10,8 +10,12 @@ logger = logging.getLogger(__name__)
 
 
 def caption(index_filename, caption_font_filename, caption_text):
-    opacity=0.50
-
+    """
+    Read the source file. Create a caption with semi-transparent backcground
+    for contrast. Paste the caption over the source image. Flatten and save.
+    This offers the illusion of transparency in the RGB image that does not
+    support transparency.
+    """
     # Want just the base portion of the filename without extension. Will also
     # be used to name the target file.
     index_filename_base = os.path.splitext(os.path.basename(index_filename))[0]
@@ -21,30 +25,39 @@ def caption(index_filename, caption_font_filename, caption_text):
     index_dirname = os.path.dirname(index_filename)
     target_filename = os.path.join(index_dirname, target_basename)
 
+    # Initialize some symbolic color names.
     black = (0, 0, 0)
     white = (255, 255, 255)
     transparent = (0, 0, 0, 0)
+
+    # Create an RGBA image object from the source file.
     index_img = Image.open(index_filename).convert('RGBA')
     width, height = index_img.size
-    caption_bg_color = (0, 0, 0, 100)
-    caption_bg_height = int(height / 20)
-    caption_color = "white"
-    caption_font_size = int(caption_bg_height * 0.5)
 
+    # Caption height is 5% of total image height.
+    caption_bg_color = (0, 0, 0, 100)   # Semi-transparent caption background
+    caption_bg_height = int(height / 20)
+    caption_font_size = int(caption_bg_height * 0.5)
     caption_font = ImageFont.truetype(
             caption_font_filename, caption_font_size)
-    wm = Image.new('RGBA',(width, caption_bg_height), caption_bg_color)
 
+    # New RGBA image object for the caption.
+    wm = Image.new('RGBA',(width, caption_bg_height), caption_bg_color)
     draw = ImageDraw.Draw(wm)
-    caption_width, caption_height = draw.textsize(
-            caption_text, caption_font)
+    caption_color = "white"
+    caption_width, caption_height = draw.textsize(caption_text, caption_font)
     draw.text(((width-caption_width)/2, (caption_bg_height-caption_height)/2),
             caption_text, caption_color, caption_font)
 
+    # Paste the caption on the lower end of the source image object.
     en = ImageEnhance.Brightness(wm)
+    opacity=0.50    # TODO: Clumsy
     mask = en.enhance(1-opacity)
     index_img.paste(wm, (0, height-caption_bg_height), mask)
+
+    # Flatten alpha channel and save.
     index_img.convert("RGB").save(target_filename)
+
 
 class FormArgumentParser(argparse.ArgumentParser):
     """
